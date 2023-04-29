5afaa0f1b877ec0caa59a85497b3a2843ed8c10b
robin <robinnagpal.tiet@gmail.com>
2023-04-29 07:38:57 -0400

    chnage env variables

--- nema-server/src/documentInfo/helpers/getPresignedUrl.ts
+++ nema-server/src/documentInfo/helpers/getPresignedUrl.ts
@@ -4,31 +4,29 @@ import { ObjectCannedACL, PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { S3 } from 'aws-sdk';
 
-export const s3Config = {
-  bucketName: String(process.env.PUBLIC_AWS_S3_BUCKET),
-  defaultRegion: String(process.env.AWS_DEFAULT_REGION),
+const s3Config = {
+  credentials: {
+    accessKeyId: String(process.env.NEMA_AWS_ACCESS_KEY_ID),
+    secretAccessKey: String(process.env.NEMA_AWS_SECRET_ACCESS_KEY),
+  },
+  region: process.env.AWS_DEFAULT_REGION,
 };
 
+console.log('s3Config', s3Config);
 export class PresignedUrlCreator {
   private client: S3;
 
-  private readonly bucketName = s3Config.bucketName;
-
   constructor() {
-    this.client = new S3({
-      region: s3Config.defaultRegion,
-    });
+    this.client = new S3(s3Config);
   }
 
   async createSignedUrl(spaceId: string, namespace: string, args: CreateSignedUrlInput): Promise<string> {
     const { contentType, name } = args;
 
-    const client = new S3Client({
-      region: s3Config.defaultRegion,
-    });
+    const client = new S3Client(s3Config);
     const command = new PutObjectCommand({
       Key: `${spaceId}/temp-uploads/${namespace}/${Date.now()}_${slugify(name)}`,
-      Bucket: s3Config.bucketName,
+      Bucket: process.env.NEMA_PUBLIC_AWS_S3_BUCKET,
       ContentType: contentType,
       ACL: ObjectCannedACL.public_read,
     });
