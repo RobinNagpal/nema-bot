bfc6e37f38ea2a7eb2569b713632acf8349159ca
NikhilGithubOfficial <nkmnikhil0009@gmail.com>
2023-04-30 21:24:39 +0530

    tried autonomous agents

--- nema-client/src/pages/api/chat.ts
+++ nema-client/src/pages/api/chat.ts
@@ -1,12 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { PineconeClient } from "@pinecone-database/pinecone";
+import { PineconeClient } from '@pinecone-database/pinecone';
 import * as Ably from 'ably';
-import { CallbackManager } from "langchain/callbacks";
-import { LLMChain } from "langchain/chains";
-import { ChatOpenAI } from "langchain/chat_models";
+import { CallbackManager } from 'langchain/callbacks';
+import { LLMChain } from 'langchain/chains';
+import { ChatOpenAI } from 'langchain/chat_models';
 import { OpenAIEmbeddings } from 'langchain/embeddings';
-import { OpenAI } from "langchain/llms";
-import { PromptTemplate } from "langchain/prompts";
+import { OpenAI } from 'langchain/llms';
+import { PromptTemplate } from 'langchain/prompts';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { uuid } from 'uuidv4';
 import { summarizeLongDocument } from './summarizer';
@@ -15,9 +15,29 @@ import { ConversationLog } from './conversationLog';
 import { Metadata, getMatchesFromEmbeddings } from './matches';
 import { templates } from './templates';
 
+import { AutoGPT } from 'langchain/experimental/autogpt';
+// import { ReadFileTool, WriteFileTool, SerpAPI } from 'langchain/tools';
+// import { NodeFileStore } from 'langchain/stores/file/node';
+// import { HNSWLib } from 'langchain/vectorstores/hnswlib';
+
+import { BabyAGI } from 'langchain/experimental/babyagi';
+import { MemoryVectorStore } from 'langchain/vectorstores/memory';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
 
 const llm = new OpenAI({});
-let pinecone: PineconeClient | null = null
+let pinecone: PineconeClient | null = null;
+
+// const store = new NodeFileStore();
+
+// const tools = [
+//   new ReadFileTool({ store }),
+//   new WriteFileTool({ store }),
+//   new SerpAPI(process.env.SERPAPI_API_KEY, {
+//     location: 'San Francisco,California,United States',
+//     hl: 'en',
+//     gl: 'us',
+//   }),
+// ];
 
 const initPineconeClient = async () => {
   pinecone = new PineconeClient();
@@ -25,11 +45,11 @@ const initPineconeClient = async () => {
     environment: process.env.PINECONE_ENVIRONMENT!,
     apiKey: process.env.PINECONE_API_KEY!,
   });
-}
+};
 
 const ably = new Ably.Realtime({ key: process.env.ABLY_API_KEY });
 
-const handleRequest = async ({ prompt, userId }: { prompt: string, userId: string }) => {
+const handleRequest = async ({ prompt, userId }: { prompt: string; userId: string }) => {
   if (!pinecone) {
     await initPineconeClient();
   }
@@ -38,60 +58,67 @@ const handleRequest = async ({ prompt, userId }: { prompt: string, userId: strin
 
   try {
     const channel = ably.channels.get(userId);
-    const interactionId = uuid()
+    const interactionId = uuid();
 
     // Retrieve the conversation log and save the user's prompt
-    const conversationLog = new ConversationLog(userId)
-    const conversationHistory = await conversationLog.getConversation({ limit: 10 })
-    await conversationLog.addEntry({ entry: prompt, speaker: "user" })
+    const conversationLog = new ConversationLog(userId);
+    const conversationHistory = await conversationLog.getConversation({ limit: 10 });
+    await conversationLog.addEntry({ entry: prompt, speaker: 'user' });
 
     // Build an LLM chain that will improve the user prompt
     const inquiryChain = new LLMChain({
-      llm, prompt: new PromptTemplate({
+      llm,
+      prompt: new PromptTemplate({
         template: templates.inquiryTemplate,
-        inputVariables: ["userPrompt", "conversationHistory"],
-      })
+        inputVariables: ['userPrompt', 'conversationHistory'],
+      }),
     });
-    const inquiryChainResult = await inquiryChain.call({ userPrompt: prompt, conversationHistory })
-    const inquiry = inquiryChainResult.text
+    const inquiryChainResult = await inquiryChain.call({ userPrompt: prompt, conversationHistory });
+    const inquiry = inquiryChainResult.text;
 
     // Embed the user's intent and query the Pinecone index
     const embedder = new OpenAIEmbeddings();
     channel.publish({
       data: {
-        event: "status",
-        message: "Embedding your inquiry...",
-      }
-    })
+        event: 'status',
+        message: 'Embedding your inquiry...',
+      },
+    });
 
     const embeddings = await embedder.embedQuery(inquiry);
     channel.publish({
       data: {
-        event: "status",
-        message: "Finding matches...",
-      }
-    })
+        event: 'status',
+        message: 'Finding matches...',
+      },
+    });
 
-    console.log('embeddings', embeddings.length)
+    console.log('embeddings', embeddings.length);
     const matches = await getMatchesFromEmbeddings(embeddings, pinecone!, 3);
 
-    console.log('matches', matches.length)
+    console.log('matches', matches.length);
     channel.publish({
       data: {
-        event: "status",
+        event: 'status',
         message: `Found ${matches?.length} matches`,
-      }
-    })
+      },
+    });
 
     // const urls = docs && Array.from(new Set(docs.map(doc => doc.metadata.url)))
 
-    const urls = matches && Array.from(new Set(matches.map(match => {
-      const metadata = match.metadata as Metadata
-      const { url } = metadata
-      return url
-    })))
+    const urls =
+      matches &&
+      Array.from(
+        new Set(
+          matches.map((match) => {
+            const metadata = match.metadata as Metadata;
+            const { url } = metadata;
+            return url;
+          })
+        )
+      );
 
-    console.log(urls)
+    console.log(urls);
 
     // const fullDocuments = matches && Array.from(new Set(matches.map(match => {
     //   const metadata = match.metadata as Metadata
@@ -99,113 +126,157 @@ const handleRequest = async ({ prompt, userId }: { prompt: string, userId: strin
     //   return text
     // })))
 
-    const fullDocuments = matches && Array.from(
-      matches.reduce((map, match) => {
-        const metadata = match.metadata as Metadata;
-        const { text, url } = metadata;
-        if (!map.has(url)) {
-          map.set(url, text);
-        }
-        return map;
-      }, new Map())
-    ).map(([_, text]) => text);
+    const fullDocuments =
+      matches &&
+      Array.from(
+        matches.reduce((map, match) => {
+          const metadata = match.metadata as Metadata;
+          const { text, url } = metadata;
+          if (!map.has(url)) {
+            map.set(url, text);
+          }
+          return map;
+        }, new Map())
+      ).map(([_, text]) => text);
 
-    const chunkedDocs = matches && Array.from(new Set(matches.map(match => {
-      const metadata = match.metadata as Metadata
-      const { chunk } = metadata
-      return chunk
-    })))
+    const chunkedDocs =
+      matches &&
+      Array.from(
+        new Set(
+          matches.map((match) => {
+            const metadata = match.metadata as Metadata;
+            const { chunk } = metadata;
+            return chunk;
+          })
+        )
+      );
 
     // const fullDocuments = urls && await getDocumentsByUrl(urls)
     // console.log(fullDocuments)
 
     channel.publish({
       data: {
-        event: "status",
-        message: `Documents are summarized (they are ${fullDocuments?.join("").length} long)`,
-      }
-    })
+        event: 'status',
+        message: `Documents are summarized (they are ${fullDocuments?.join('').length} long)`,
+      },
+    });
 
     const onSummaryDone = (summary: string) => {
-      summarizedCount += 1
+      summarizedCount += 1;
 
       channel.publish({
         data: {
-          event: "status",
+          event: 'status',
           message: `Done summarizing ${summarizedCount} documents`,
-        }
-      })
-    }
+        },
+      });
+    };
 
-    const summary = await summarizeLongDocument(fullDocuments!.join("\n"), inquiry, onSummaryDone)
-    console.log(summary)
+    const summary = await summarizeLongDocument(fullDocuments!.join('\n'), inquiry, onSummaryDone);
+    console.log('summary:', summary);
 
     // const summary = chunkedDocs!.join("\n")
 
     channel.publish({
       data: {
-        event: "status",
+        event: 'status',
         message: `Documents are summarized. Forming final answer...`,
-      }
-    })
+      },
+    });
     // Prepare a QA chain and call it with the document summaries and the user's prompt
     const promptTemplate = new PromptTemplate({
       template: templates.qaTemplate,
-      inputVariables: ["summaries", "question", "conversationHistory", "urls"],
+      inputVariables: ['summaries', 'question', 'conversationHistory', 'urls'],
     });
 
+    // const chat = new ChatOpenAI({
+    //   streaming: true,
+    //   verbose: true,
+    //   modelName: 'gpt-3.5-turbo',
+    //   callbackManager: CallbackManager.fromHandlers({
+    //     async handleLLMNewToken(token) {
+    //       console.log(token);
+    //       channel.publish({
+    //         data: {
+    //           event: 'response',
+    //           token: token,
+    //           interactionId,
+    //         },
+    //       });
+    //     },
+    //     async handleLLMEnd(result) {
+    //       channel.publish({
+    //         data: {
+    //           event: 'responseEnd',
+    //           token: 'END',
+    //           interactionId,
+    //         },
+    //       });
+    //     },
+    //   }),
+    // });
 
-    const chat = new ChatOpenAI({
-      streaming: true,
-      verbose: true,
-      modelName: "gpt-3.5-turbo",
-      callbackManager: CallbackManager.fromHandlers({
-        async handleLLMNewToken(token) {
-          console.log(token)
-          channel.publish({
-            data: {
-              event: "response",
-              token: token,
-              interactionId
-            }
-          })
-        },
-        async handleLLMEnd(result) {
-          channel.publish({
-            data: {
-              event: "responseEnd",
-              token: "END",
-              interactionId
-            }
-          })
-        }
-      }),
+    // const chain = new LLMChain({
+    //   prompt: promptTemplate,
+    //   llm: chat,
+    // });
+
+    // await chain.call({
+    //   summaries: summary,
+    //   question: prompt,
+    //   conversationHistory,
+    //   urls,
+    // });
+
+    const vectorStore = new MemoryVectorStore(new OpenAIEmbeddings());
+
+    // const pineconeIndex = pinecone?.Index(process.env.PINECONE_INDEX!);
+    // const vectorStore = await PineconeStore.fromExistingIndex(new OpenAIEmbeddings(), { pineconeIndex });
+
+    const babyAGI = BabyAGI.fromLLM({
+      llm: new OpenAI({ temperature: 0 }),
+      vectorstore: vectorStore,
+      maxIterations: 3,
     });
 
-    const chain = new LLMChain({
-      prompt: promptTemplate,
-      llm: chat,
-    });
+    await babyAGI.call({ objective: prompt });
+    // const response = await babyAGI.call({ objective: 'What is impermanent loss?' });
+    // console.log('babyAGI:', response);
 
-    await chain.call({
-      summaries: summary,
-      question: prompt,
-      conversationHistory,
-      urls
-    });
+    // const autogpt = AutoGPT.fromLLMAndTools(
+    //     new ChatOpenAI({ temperature: 0 }),
+    //     tools,
+    //     {
+    //       memory: vectorStore.asRetriever(),
+    //       aiName: "Tom",
+    //       aiRole: "Assistant",
+    //     }
+    //   );
 
+    //   await autogpt.run(["write a weather report for SF today"]);
+
+    // const vectorStore = new HNSWLib(new OpenAIEmbeddings(), {
+    //   space: 'cosine',
+    //   numDimensions: 1536,
+    // });
+
+    // const autogpt = AutoGPT.fromLLMAndTools(new ChatOpenAI({ temperature: 0 }), tools, {
+    //   memory: vectorStore.asRetriever(),
+    //   aiName: 'Tom',
+    //   aiRole: 'Assistant',
+    // });
+
+    // const response = await autogpt.run(['write a weather report for SF today']);
+    // console.log('autogpt: ', response);
   } catch (error) {
     //@ts-ignore
-    console.error(error)
+    console.error(error);
   }
-}
+};
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { body } = req;
   const { prompt, userId } = body;
-  await handleRequest({ prompt, userId })
-  res.status(200).json({ "message": "started" })
+  await handleRequest({ prompt, userId });
+  res.status(200).json({ message: 'started' });
 }
