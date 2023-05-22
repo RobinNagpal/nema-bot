554224a62a8332634440e885e348f6916962e0d0
robin <robinnagpal.tiet@gmail.com>
2023-05-22 14:16:05 -0400

    fix styling

--- nema-server/src/prompts/summarize/createSummary.ts
+++ nema-server/src/prompts/summarize/createSummary.ts
@@ -1,9 +1,6 @@
-import { uniswapGitbooks } from '@/contents/uniswapV3Contents';
-import { indexDocument } from '@/indexer/indexDocument';
-import { PineconeClient } from '@pinecone-database/pinecone';
-import { Configuration,OpenAIApi } from 'openai';
-import { UnstructuredLoader } from 'langchain/document_loaders';
 import dotenv from 'dotenv';
+import { Configuration, OpenAIApi } from 'openai';
+
 dotenv.config();
 
 // let pinecone: PineconeClient | null = null;
@@ -17,11 +14,10 @@ dotenv.config();
 // };
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
-
 
 export default async function createSummary() {
   // 1) Update README.md to include all the missing steps and how to test if the local
@@ -36,62 +32,54 @@ export default async function createSummary() {
   // 5) Write a prompt to get the summary of the chunk
   // 6) Combine all the summaries and resummarize it. This is the final summary
 
-
   // Some of  the things we need to consider
   // There is something call map reduce chain, which can make this process faster
   // Prompts needs to be created carefully.
 
   // const index = pinecone!.Index(process.env.PINECONE_INDEX_NAME!);
 
-
   // This
   // await indexDocument(uniswapGitbooks[0], index)
-  
-  const inputchunk:Array<String|undefined> =["The Internet of Things offers many opportunities to grow the economy and improve quality of life. Just as the public sector was instrumental in enabling the development and deployment of the Internet, it must play a similar role to ensure the success of the Internet of Things. Therefore, national governments should create comprehensive national strategies for the Internet of Things to ensure that the technology develops cohesively and rapidly, that consumers and businesses do not face barriers to adoption, and that both the private and public sector take full advantage of the coming wave of smart devices.","The Internet has turned our existence upside down. It has revolutionized communications, to the extent that it is now our preferred medium of everyday communication. In almost everything we do, we use the Internet. Ordering a pizza, buying a television, sharing a moment with a friend, sending a picture over instant messaging. Before the Internet, if you wanted to keep up with the news, you had to walk down to the newsstand when it opened in the morning and buy a local edition reporting what had happened the previous day."]
-  const outputchunk:Array<String|undefined> =[]
-  
-  async function generateSummary(chunk:String|undefined){
+
+  const inputchunk: Array<string | undefined> = [
+    'The Internet of Things offers many opportunities to grow the economy and improve quality of life. Just as the public sector was instrumental in enabling the development and deployment of the Internet, it must play a similar role to ensure the success of the Internet of Things. Therefore, national governments should create comprehensive national strategies for the Internet of Things to ensure that the technology develops cohesively and rapidly, that consumers and businesses do not face barriers to adoption, and that both the private and public sector take full advantage of the coming wave of smart devices.',
+    'The Internet has turned our existence upside down. It has revolutionized communications, to the extent that it is now our preferred medium of everyday communication. In almost everything we do, we use the Internet. Ordering a pizza, buying a television, sharing a moment with a friend, sending a picture over instant messaging. Before the Internet, if you wanted to keep up with the news, you had to walk down to the newsstand when it opened in the morning and buy a local edition reporting what had happened the previous day.',
+  ];
+  const outputchunk: Array<string | undefined> = [];
+
+  async function generateSummary(chunk: string | undefined) {
     const prompt = `${chunk}\n\nTl;dr`;
     const response = await openai.createCompletion({
-      model: "text-davinci-003",
-  prompt: prompt,
-  temperature: 0,
-  max_tokens: 130,
-  top_p: 1.0,
-  frequency_penalty: 0.2,
-  presence_penalty: 0.2,
+      model: 'text-davinci-003',
+      prompt: prompt,
+      temperature: 0,
+      max_tokens: 130,
+      top_p: 1.0,
+      frequency_penalty: 0.2,
+      presence_penalty: 0.2,
     });
     console.log(response);
-    const summary:String | undefined = response.data.choices[0].text?.trim()
-    return summary
-  
+    const summary: string | undefined = response.data.choices[0].text?.trim();
+    return summary;
   }
 
   async function runLoop() {
     await Promise.all(
       inputchunk.map(async (chunk) => {
-        console.log("length of the input: ",chunk?.length);
+        console.log('length of the input: ', chunk?.length);
         const output = await generateSummary(chunk);
-        console.log("this is the output:", output?.substring(2));
+        console.log('this is the output:', output?.substring(2));
         await outputchunk.push(output?.substring(2));
       })
     );
-  
-    const initialSummary = outputchunk.join(" ");
+
+    const initialSummary = outputchunk.join(' ');
     const finalSummary = await generateSummary(initialSummary);
-  
-    console.log("this is initial summary:", initialSummary);
-    console.log("this is final summary:", finalSummary);
+
+    console.log('this is initial summary:', initialSummary);
+    console.log('this is final summary:', finalSummary);
   }
-  
+
   runLoop();
-
-
-      
 }
 createSummary();
-
-
-
-
-
