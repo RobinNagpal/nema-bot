104f990596730f89adb1ee6b2f3d99bfc5294983
harisudarsan <sudarshanhari561@gmail.com>
2023-05-26 14:17:35 +0530

    removed the previous comments

--- nema-server/src/prompts/summarize/createSummary.ts
+++ nema-server/src/prompts/summarize/createSummary.ts
@@ -4,15 +4,6 @@ import { Configuration, OpenAIApi } from 'openai';
 
 dotenv.config();
 
-// let pinecone: PineconeClient | null = null;
-
-// const initPineconeClient = async () => {
-//   pinecone = new PineconeClient();
-//   await pinecone.init({
-//     environment: process.env.PINECONE_ENVIRONMENT!,
-//     apiKey: process.env.PINECONE_API_KEY!,
-//   });
-// };
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
@@ -36,26 +27,7 @@ async function generateSummary(chunk: string | undefined) {
 }
 
 export default async function createSummary() {
-  // 1) Update README.md to include all the missing steps and how to test if the local
-  // setup is working
 
-  // 2) Setup pinecone
-  // Read gitbook contents. Make sure gitbook contents are not having HTML or CSS.
-  // Also read PDF doc and index it
-
-  // 3) We read all the contents as string
-  // 4) We chunk the string into 12-20k characters
-  // 5) Write a prompt to get the summary of the chunk
-  // 6) Combine all the summaries and resummarize it. This is the final summary
-
-  // Some of  the things we need to consider
-  // There is something call map reduce chain, which can make this process faster
-  // Prompts needs to be created carefully.
-
-  // const index = pinecone!.Index(process.env.PINECONE_INDEX_NAME!);
-
-  // This
-  // await indexDocument(uniswapGitbooks[0], index)
 
   const scrappedContent = await doScaping();
   const inputchunks: string[] = Object.values(scrappedContent);
